050161bae050e38abda11b004aae8db0119e1a59
Vaibhab Tiwari <vaibhabtiwari9887@gmail.com>
2023-04-02 23:14:36 +0530

    deployement test

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import mentorsRoutes from "./routes/mentorsRoutes.js";
 import studentsRoutes from "./routes/studentsRoutes.js";
 
 const app = express();
-
+dotenv.config();
 app.use(cors());
 
 //these variables come from dotenv file but for now we are hardcoding it
-const CONNECTION_URL = "mongodb://127.0.0.1:27017/cscorner";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.port || 5000;
 
 //connecting to the database
